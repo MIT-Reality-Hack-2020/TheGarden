3468f8767f88202b612422ea30c2970335462618
taeyonii <taeyonii@gmail.com>
2020-01-19 14:05:24 -0500

    committt
    Former-commit-id: 7b36e0cbc53fc542f322e5486568663d9e0f0cb5

---
+++ Assets/GPSlocation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GPSlocation : MonoBehaviour
+{
+
+    public float longitude;
+    public float latitude;
+    public Text gpsText;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        if (Input.location.isEnabledByUser)
+            StartCoroutine(GetLocation());
+        if (!Input.location.isEnabledByUser)
+            yield break;
+
+    }
+    private IEnumerator GetLocation()
+    {
+        Input.location.Start();
+        while(Input.location.status == LocationServiceStatus.Initializing)
+        {
+            yield return new WaitForSeconds(1);
+        }
+        longitude = Input.location.lastData.longitude;
+        latitude = Input.location.lastData.latitude;
+        yield break;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        longitude = Input.location.lastData.longitude;
+        latitude = Input.location.lastData.latitude;
+        gpsText.text = "Lat: " + latitude + "\n Lon: " + longitude;
+    }
+}
